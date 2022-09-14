f2e5bd4d2c0dfeab91aace2bdb592a48203b14d5
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-14 21:55:23 +0400

    initial commit

---
+++ src/test/java/com/example/springquick/SpringQuickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springquick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
