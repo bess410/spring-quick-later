933d5fa99306f2236a0a253e17593515b430d47c
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-23 01:36:09 +0400

    sq-ch11-ex1 using RestTemplate

--- src/main/java/com/example/springquick/proxy/PaymentsProxy.java
+++ src/main/java/com/example/springquick/proxy/PaymentsProxy.java
@@ -1,17 +1,42 @@
 package com.example.springquick.proxy;
 
 import com.example.springquick.dto.Payment;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
-@FeignClient(name = "payments",
-             url = "${name.service.url}")
-public interface PaymentsProxy {
+import java.util.UUID;
 
-    @PostMapping("/payment")
-    Payment createPayment(
-            @RequestHeader String requestId,
-            @RequestBody Payment payment);
+@Data
+@Component
+public class PaymentsProxy {
+
+    private final RestTemplate rest = new RestTemplate();
+
+    @Value("${name.service.url}")
+    private String paymentsServiceUrl;
+
+    public Payment createPayment(Payment payment) {
+        String uri = paymentsServiceUrl + "/payment";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("requestId", UUID.randomUUID().toString());
+
+        HttpEntity<Payment> httpEntity = new HttpEntity<>(payment, headers);
+
+        ResponseEntity<Payment> response =
+                rest.exchange(uri,
+                        HttpMethod.POST,
+                        httpEntity,
+                        Payment.class);
+
+        return response.getBody();
+    }
 }
