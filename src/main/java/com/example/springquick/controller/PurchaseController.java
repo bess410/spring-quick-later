bc72045dd5f3947f7ad73ac8762e7ab635bfafa6
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-26 00:55:00 +0400

    sq-ch12-ex1 using JdbcTemplate to work with H2

---
+++ src/main/java/com/example/springquick/controller/PurchaseController.java
@@ -0,0 +1,30 @@
+package com.example.springquick.controller;
+
+import com.example.springquick.model.Purchase;
+import com.example.springquick.repository.PurchaseRepository;
+import lombok.Data;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Data
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
