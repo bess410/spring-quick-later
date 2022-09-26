025490d9d2afeff2ac9b3be28febc0da34bb1e58
Andrey Sterkhov <asterkhov@65apps.com>
2022-09-26 21:47:14 +0400

    sq-ch12-ex2 using JdbcTemplate to work with postgres

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,5 @@
 create table if not exists purchase (
-    id int auto_increment primary key,
+    id serial primary key,
     product varchar(50) not null,
-    price double not null
+    price decimal not null
 );
