7858247ef249f8b48c2d772283bc269977d31730
Andrey Sterkhov <asterkhov@65apps.com>
2022-10-01 01:31:43 +0400

    sq-ch15-ex2 using annotations for mocking + example with throwing an exeption

---
+++ src/test/java/com/example/springquick/TransferServiceWithAnnotationsUnitTests.java
@@ -0,0 +1,62 @@
+package com.example.springquick;
+
+import com.example.springquick.exception.AccountNotFoundException;
+import com.example.springquick.model.Account;
+import com.example.springquick.repository.AccountRepository;
+import com.example.springquick.service.TransferService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TransferServiceWithAnnotationsUnitTests {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private TransferService transferService;
+
+    @Test
+    public void moneyTransferHappyFlow() {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        Account destination = new Account();
+        destination.setId(2);
+        destination.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findById(sender.getId())).willReturn(Optional.of(sender));
+        given(accountRepository.findById(destination.getId())).willReturn(Optional.of(destination));
+
+        transferService.transferMoney(1, 2, new BigDecimal(100));
+
+        verify(accountRepository).changeAmount(1, new BigDecimal(900));
+        verify(accountRepository).changeAmount(2, new BigDecimal(1100));
+    }
+
+    @Test
+    public void moneyTransferDestinationAccountNotFoundFlow() {
+        Account sender = new Account();
+        sender.setId(1);
+        sender.setAmount(new BigDecimal(1000));
+
+        given(accountRepository.findById(1L)).willReturn(Optional.of(sender));
+        given(accountRepository.findById(2L)).willReturn(Optional.empty());
+
+        assertThrows(AccountNotFoundException.class,
+                () -> transferService.transferMoney(1, 2, new BigDecimal(100)));
+
+        verify(accountRepository, never()).changeAmount(anyLong(), any());
+    }
+}
